9007b3cd48442cff6e8c8ea0926b377704e42e53
evilb <evilb@syneforge.com>
2022-04-02 13:27:44 +0300

    Initial Commit

---
+++ BeautifulAnimanion/BeautifulAnimanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeautifulAnimanionApp.swift
+//  BeautifulAnimanion
+//
+//  Created by evilb on 02.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct BeautifulAnimanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
